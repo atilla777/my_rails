6f2d33584d5baa6b85fb15990b732fed30fb2dc9
Aleksey Slivka <slivka77@inbox.ru>
2017-04-18 10:55:21 +0300

    Add params to link_to_sort

--- application_helper.rb
+++ application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def link_to_sort(name, field, form, cont = controller.controller_name, act = controller.action_name)
+  def link_to_sort(name, field, form, params = {}, cont = controller.controller_name, act = controller.action_name)
     if form.sort_field == field
       direction_image = form.sort_direction == :asc ? 'glyphicon-triangle-top' : 'glyphicon-triangle-bottom'
       direction_image = %Q(<i class ='glyphicon #{direction_image}' aria-hidden='true'></i>)
@@ -9,7 +9,7 @@ module ApplicationHelper
             action: act,
             filter: form.filter,
             sort_field: field,
-            sort_direction: new_sort_direction},
+            sort_direction: new_sort_direction}.merge(params),
             {remote: true,
             class: 'btn btn-link'}) do
       %Q(#{name}#{direction_image}).html_safe
