795a0382679870010f700b2f681e2b0d4ae0ee58
Aleksey Slivka <slivka77@inbox.ru>
2017-04-21 08:15:22 +0300

    Refactor sort

--- application_helper.rb
+++ application_helper.rb
@@ -1,18 +1,20 @@
 module ApplicationHelper
-  def link_to_sort(name, field, form, params = {}, cont = controller.controller_name, act = controller.action_name)
-    if form.sort_field == field
-      direction_image = form.sort_direction == :asc ? 'glyphicon-triangle-top' : 'glyphicon-triangle-bottom'
+  def link_to_sort(options)
+    options[:controller] ||= controller.controller_name
+    options[:action] ||= controller.action_name
+    if options[:form].sort_field == options[:field_name]
+      direction_image = options[:form].sort_direction == :asc ? 'glyphicon-triangle-top' : 'glyphicon-triangle-bottom'
       direction_image = %Q(<i class ='glyphicon #{direction_image}' aria-hidden='true'></i>)
     end
-    new_sort_direction = form.sort_direction == :asc ? :desc : :asc
-    link_to({controller: cont,
-            action: act,
-            filter: form.filter,
-            sort_field: field,
-            sort_direction: new_sort_direction}.merge(params),
+    new_sort_direction = options[:form].sort_direction == :asc ? :desc : :asc
+    link_to({controller: options[:controller],
+            action: options[:action],
+            filter: options[:form].filter,
+            sort_field: options[:field_name],
+            sort_direction: new_sort_direction},
             {remote: true,
             class: 'btn btn-link'}) do
-      %Q(#{name}#{direction_image}).html_safe
+      %Q(#{options[:field_label]}#{direction_image}).html_safe
     end
   end
 end
