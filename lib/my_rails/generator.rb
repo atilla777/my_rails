95ffa440d8d954bd5a84daffdda9b740b7c1df8d
Aleksey Slivka <slivka77@inbox.ru>
2017-09-10 09:11:48 +0300

    Rename template folder

--- lib/my_rails/generator.rb
+++ lib/my_rails/generator.rb
@@ -1,13 +1,14 @@
 require 'thor'
+
 module MyRails
   class Generator < Thor
-    include ThorActions
+    include Thor::Actions
 
     def self.source_root
-      File.dirname(__FILE__) << '/generator'
+      File.dirname(__FILE__) << '/generator/templates'
     end
 
-    describe 'create rails template file (my_rails.rb) for use in rails new <name> -m my_rails.rb'
+    desc :init, 'create rails template file (my_rails.rb) for use in rails new <name> -m my_rails.rb'
     def init
       copy_file 'my_rails.rb'
     end
